d1acddfbae77ba120e06d257d5c00ba2e6e468b3
David Lopez Vega <lopez.da.ve2511@gmail.com>
2026-08-21 19:32:38 -0700

    Ejercicio Realizado Proyecto Final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -3,15 +3,79 @@ package www.cibertec;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        int cantidad = 10;
+        int marca = 0;
+        double precio = obtenerPrecio(marca);
+        double importeCompra = obtenerImporteCompra_v2(precio, cantidad);
+        double importeDescuento = obtenerImporteDescuento(importeCompra, cantidad);
+        double importePagar = obtenerImportePagar(importeCompra, importeDescuento);
+        int lapicero = calculoLapiceros(importePagar, cantidad);
+        imprimir(importeCompra, importePagar, lapicero, importeDescuento);
+    }
+
+    static double obtenerPrecio(int mar) {
+        double precio;
+        if (mar == 0) {
+            precio = 5.2;
+        } else if (mar == 1) {
+            precio = 8.2;
+        } else if (mar == 2) {
+            precio = 6.5;
+        } else {
+            precio = 7.4;
         }
+        return precio;
+    }
+
+    static double obtenerImporteCompra_v1(double preci, int canti) {
+        double importeCompra;
+        importeCompra = preci * canti;
+        return importeCompra;
+    }
+
+    static double obtenerImporteCompra_v2(double preci, int canti) {
+        return preci * canti;
+    }
+
+    static double obtenerImporteDescuento(double importeCompra, int cantidad) {
+        double importeDescuento;
+        if (cantidad < 18) {
+            importeDescuento = importeCompra * 0.125;
+        } else if (cantidad >= 12 && cantidad < 18) {
+            importeDescuento = importeCompra * 0.10;
+        } else if (cantidad >= 6 && cantidad < 12) {
+            importeDescuento = importeCompra * 0.075;
+        } else {
+            importeDescuento = importeCompra * 0.0;
+        }
+        return importeDescuento;
+    }
+
+    static double obtenerImportePagar(double importeCompra, double importeDescuento) {
+        return importeCompra - importeDescuento;
+    }
+
+    static int calculoLapiceros(double importePagar, int cantidad) {
+        int lapicero;
+        if (importePagar < 12) {
+            lapicero = cantidad * 2;
+        } else if (importePagar >= 12 && importePagar < 24) {
+            lapicero = cantidad * 4;
+        } else if (importePagar >= 24 && importePagar < 36) {
+            lapicero = 1 * cantidad;
+        } else {
+            lapicero = 2 * cantidad;
+        }
+        return lapicero;
+    }
+
+    static void imprimir(double importeCompra, double importePagar, int lapicero, double importeDescuento) {
+        System.out.println("SALIDA DE RESULTADO\n");
+        System.out.println("importe de compra : " + importeCompra);
+        System.out.println("importe de descuento: " + importeDescuento);
+        System.out.println("importe a pagar : " + importePagar);
+        System.out.println("lapiceros : " + lapicero);
     }
 }
